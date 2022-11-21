96a2ecb8c9edbe0fd36459d6a59daa91c8ca6f26
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-21 15:45:06 +0530

    assingment-02 - solution

--- 03-constructs/assignment-02.go
+++ 03-constructs/assignment-02.go
@@ -20,3 +20,44 @@
 		print "Invalid choice"
 		display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, n1, n2, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice :")
+		fmt.Scanln(&userChoice)
+
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+
+		if userChoice == 5 {
+			fmt.Println("Thank you")
+			break
+		}
+
+		fmt.Println("Enter 2 numbers :")
+		fmt.Scanln(&n1, &n2)
+		switch userChoice {
+		case 1:
+			result = n1 + n2
+		case 2:
+			result = n1 - n2
+		case 3:
+			result = n1 * n2
+		case 4:
+			result = n1 / n2
+		}
+		fmt.Println("Result :", result)
+	}
+}
