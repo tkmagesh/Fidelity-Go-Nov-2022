1a11c2d5fefd224c8677de596f7ceb441ad58648
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-21 16:38:59 +0530

    assignment-03 solution

---
+++ 04-functions/assignment-03.go
@@ -0,0 +1,73 @@
+/*
+	Refactor the solution for assignment-02 into functions with each function having ONLY ONE responsibility
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, n1, n2 int
+	for {
+		userChoice = getUserChoice()
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid choice")
+			continue
+		}
+
+		if userChoice == 5 {
+			fmt.Println("Thank you")
+			break
+		}
+		n1, n2 = getOperands()
+		process(userChoice, n1, n2)
+	}
+}
+
+func process(userChoice, n1, n2 int) {
+	var result int
+	switch userChoice {
+	case 1:
+		result = n1 + n2
+	case 2:
+		result = n1 - n2
+	case 3:
+		result = n1 * n2
+	case 4:
+		result = n1 / n2
+	}
+	fmt.Println("Result :", result)
+}
+func getUserChoice() int {
+	var userChoice int
+	fmt.Println("1. Add")
+	fmt.Println("2. Subtract")
+	fmt.Println("3. Multiply")
+	fmt.Println("4. Divide")
+	fmt.Println("5. Exit")
+	fmt.Println("Enter your choice :")
+	fmt.Scanln(&userChoice)
+	return userChoice
+}
+
+func getOperands() (n1, n2 int) {
+	fmt.Println("Enter 2 numbers :")
+	fmt.Scanln(&n1, &n2)
+	return
+}
+
+func add(x, y int) int {
+	return x + y
+}
+
+func subtract(x, y int) int {
+	return x - y
+}
+
+func multiply(x, y int) int {
+	return x * y
+}
+
+func divide(x, y int) int {
+	return x / y
+}
