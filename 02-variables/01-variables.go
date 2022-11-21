8842d4239a5bd1f8334e52ada67ca5fc0726f86d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-21 12:32:18 +0530

    variable declaration & initialization

--- 02-variables/01-variables.go
+++ 02-variables/01-variables.go
@@ -23,7 +23,80 @@ func main() {
 	*/
 
 	msg := "Hello World!" // := syntax can be use ONLY in function (NOT at package scope)
+	msg = "Hello everyone!"
 	fmt.Println(msg)
 
 	//var n int = 100 // CANNOT have un-used variables at function scope
+
+	//declaring multiple variables and initializing them
+	/*
+		var x int
+		var y int
+		var str string
+		var result int
+		x = 100
+		y = 200
+		str = "Sum of 100 and 200 is"
+		result = x + y
+	*/
+
+	/*
+		var x, y, result int
+		var str string
+		x = 100
+		y = 200
+		str = "Sum of 100 and 200 is"
+		result = x + y
+	*/
+
+	/*
+		var (
+			x, y, result int
+			str          string
+		)
+		x = 100
+		y = 200
+		str = "Sum of 100 and 200 is"
+		result = x + y
+	*/
+
+	/*
+		var x int = 100
+		var y int = 200
+		var str string = "Sum of 100 and 200 is"
+		var result int = x + y
+	*/
+
+	/*
+		var x, y int = 100, 200
+		var str string = "Sum of 100 and 200 is"
+		var result int = x + y
+	*/
+
+	/*
+		var (
+			x, y   int    = 100, 200
+			str    string = "Sum of 100 and 200 is"
+			result int    = x + y
+		)
+	*/
+
+	/*
+		var (
+			x, y   = 100, 200
+			str    = "Sum of 100 and 200 is"
+			result = x + y
+		)
+	*/
+
+	/*
+		var (
+			x, y, str = 100, 200, "Sum of 100 and 200 is"
+			result    = x + y
+		)
+	*/
+
+	x, y, str := 100, 200, "Sum of 100 and 200 is"
+	result := x + y
+	fmt.Println(str, result)
 }
