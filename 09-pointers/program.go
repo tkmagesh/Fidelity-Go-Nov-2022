c0ea1c86b1b04057112193f95d52d62b94324547
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-22 16:12:22 +0530

    pointers

---
+++ 09-pointers/program.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x int = 10
+	/*
+		var f float32 = 12.99
+		fmt.Println(&f)
+	*/
+	var xPtr *int = &x
+	fmt.Println(xPtr)
+
+	//dereferencing - accessing the value using the pointer
+	fmt.Println(*xPtr)
+	fmt.Println(x == *(&x))
+
+	var no int = 100
+	fmt.Println("Before incrementing, no =", no)
+	increment(&no)
+	fmt.Println("After incrementing, no =", no)
+
+	n1, n2 := 10, 20
+	fmt.Printf("Before swapping, n1 = %d and n2 = %d\n", n1, n2)
+	swap( /*  */ )
+	fmt.Printf("After swapping, n1 = %d and n2 = %d\n", n1, n2)
+}
+
+func increment(n *int) {
+	*n++
+}
+
+func swap( /*  */ ) {
+
+}
