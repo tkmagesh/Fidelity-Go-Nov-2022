33ea25884cfcfa786ace532f1aac308d1feae3db
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-22 12:22:01 +0530

    assignment-05 solution

--- 05-errors/assignment-05.go
+++ 05-errors/assignment-05.go
@@ -4,65 +4,79 @@
 
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+var InvalidUserChoiceError = errors.New("invalid user choice")
 
 func main() {
-	var userChoice, n1, n2 int
+	var n1, n2 int
 	for {
-		userChoice = getUserChoice()
-		if userChoice < 1 || userChoice > 5 {
-			fmt.Println("Invalid choice")
+		userChoice, err := getUserChoice()
+		if err != nil {
+			fmt.Println(err)
 			continue
 		}
-
 		if userChoice == 5 {
 			fmt.Println("Thank you")
 			break
 		}
+		operation, err := getOperationByUserChoice(userChoice)
+		if err == InvalidUserChoiceError {
+			fmt.Println("Invalid choice")
+			continue
+		}
 		n1, n2 = getOperands()
-		process(userChoice, n1, n2)
+		process(operation, n1, n2)
 	}
 }
 
-func process(userChoice, n1, n2 int) {
-	operation := getOperationByUserChoice(userChoice)
+func process(operation func(int, int) int, n1, n2 int) error {
 	result := operation(n1, n2)
 	fmt.Println("Result :", result)
+	return nil
 }
 
-func getOperationByUserChoice(userChoice int) func(int, int) int {
+func getOperationByUserChoice(userChoice int) (operation func(int, int) int, err error) {
 	switch userChoice {
 	case 1:
-		return add
+		operation = add
 	case 2:
-		return subtract
+		operation = subtract
 	case 3:
-		return multiply
+		operation = multiply
 	case 4:
-		return divide
+		operation = divide
 	default:
-		return func(i1, i2 int) int {
-			return 0
-		}
+		err = InvalidUserChoiceError
 	}
+	return
 }
 
-func getUserChoice() int {
-	var userChoice int
+func getUserChoice() (userChoice int, err error) {
 	fmt.Println("1. Add")
 	fmt.Println("2. Subtract")
 	fmt.Println("3. Multiply")
 	fmt.Println("4. Divide")
 	fmt.Println("5. Exit")
 	fmt.Println("Enter your choice :")
-	fmt.Scanln(&userChoice)
-	return userChoice
+	_, err = fmt.Scanln(&userChoice)
+	return
 }
 
 func getOperands() (n1, n2 int) {
-	fmt.Println("Enter 2 numbers :")
-	fmt.Scanln(&n1, &n2)
-	return
+	// https://stackoverflow.com/questions/14640218/how-to-flush-stdin-after-fmt-scanf-in-go
+	for {
+		fmt.Println("Enter 2 numbers :")
+		_, err := fmt.Scanln(&n1, &n2)
+		if err != nil {
+			fmt.Println("Error parsing the input. Please enter again")
+			continue
+		}
+		return
+	}
 }
 
 func add(x, y int) int {
