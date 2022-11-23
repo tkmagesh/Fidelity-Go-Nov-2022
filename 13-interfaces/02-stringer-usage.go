7263502e8424e91ded4462028a2911ec4a516368
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-23 13:10:13 +0530

    using the fmt.Stringer interface

---
+++ 13-interfaces/02-stringer-usage.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Category string
+}
+
+/*
+func (product Product) Format() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %f, Category = %q", product.Id, product.Name, product.Cost, product.Category)
+}
+*/
+
+//the Format() method changed to implement the fmt.Stringer interface
+func (product Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %f, Category = %q", product.Id, product.Name, product.Cost, product.Category)
+}
+
+func (product *Product) ApplyDiscount(discountPercentage float32) {
+	product.Cost = product.Cost * ((100 - discountPercentage) / 100)
+}
+
+type PerishableProduct struct {
+	Product
+	Expiry string
+}
+
+//overriding
+/*
+func (pp PerishableProduct) Format() string {
+	return fmt.Sprintf("%s, Expiry = %q", pp.Product.Format(), pp.Expiry)
+}
+*/
+
+//the Format() method changed to implement the fmt.Stringer interface
+func (pp PerishableProduct) String() string {
+	return fmt.Sprintf("%s, Expiry = %q", pp.Product, pp.Expiry)
+}
+
+func main() {
+
+	apple := PerishableProduct{
+		Product: Product{101, "Apple", 20, "Fruits"},
+		Expiry:  "5 Days",
+	}
+	fmt.Println(apple)
+
+	fmt.Println("Accessing the fields")
+	fmt.Println(apple.Product.Id, apple.Product.Name, apple.Expiry)
+	fmt.Println(apple.Name, apple.Expiry)
+
+	// fmt.Println(Format(apple.Product))
+	// fmt.Println(apple.Format())
+	// taking advantage of the fmt.Stringer interface implementation
+	fmt.Println(apple)
+	fmt.Println("After applying 10% discount")
+	apple.ApplyDiscount(10)
+	// fmt.Println(apple.Format())
+	// taking advantage of the fmt.Stringer interface implementation
+	fmt.Println(apple)
+}
+
+/* to include the Expiry in Format */
