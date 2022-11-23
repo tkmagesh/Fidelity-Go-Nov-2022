bf3e8687f4aa5490fb68077ef3679a3d32fd2523
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-23 10:34:02 +0530

    assignment-08 problem statement

---
+++ 11-structs/assignment-08.go
@@ -0,0 +1,12 @@
+/*
+Create a product struct with the following fields
+	Id
+	Name
+	Cost
+	Category
+
+Create the following functions
+	Format() => returns a formatted string of the given product as below:
+		" Id = 100, Name = "pen", Cost = 10.50, Category = "stationary" "
+	ApplyDiscount() => update the given product's cost after applying the given discount(%)
+*/
