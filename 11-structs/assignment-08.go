32a4286f8ae5235a03676d1313ebdca425e6223d
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-23 10:59:31 +0530

    assignment-08 solution

--- 11-structs/assignment-08.go
+++ 11-structs/assignment-08.go
@@ -10,3 +10,30 @@ Create the following functions
 		" Id = 100, Name = "pen", Cost = 10.50, Category = "stationary" "
 	ApplyDiscount() => update the given product's cost after applying the given discount(%)
 */
+
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float32
+	Category string
+}
+
+func main() {
+	pen := Product{100, "Pen", 10, "Stationary"}
+	fmt.Println(Format(pen))
+	fmt.Println("After applying 10% discount")
+	ApplyDiscount(&pen, 10)
+	fmt.Println(Format(pen))
+}
+
+func Format(product Product) string {
+	return fmt.Sprintf("Id = %d, Name = %q, Cost = %f, Category = %q", product.Id, product.Name, product.Cost, product.Category)
+}
+
+func ApplyDiscount(product *Product, discountPercentage float32) {
+	product.Cost = product.Cost * ((100 - discountPercentage) / 100)
+}
