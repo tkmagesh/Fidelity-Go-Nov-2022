04ffac68abea6b68b23ceda300336aa03b16d6a4
Magesh Kuppan <tkmagesh77@gmail.com>
2022-11-23 12:03:13 +0530

    more on methods

--- 12-methods/assignment-09.go
+++ 12-methods/assignment-09.go
@@ -13,18 +13,22 @@ type Product struct {
 	Category string
 }
 
-func main() {
-	pen := Product{100, "Pen", 10, "Stationary"}
-	fmt.Println(Format(pen))
-	fmt.Println("After applying 10% discount")
-	ApplyDiscount(&pen, 10)
-	fmt.Println(Format(pen))
-}
-
-func Format(product Product) string {
+func (product Product) Format() string {
 	return fmt.Sprintf("Id = %d, Name = %q, Cost = %f, Category = %q", product.Id, product.Name, product.Cost, product.Category)
 }
 
-func ApplyDiscount(product *Product, discountPercentage float32) {
+func (product *Product) ApplyDiscount(discountPercentage float32) {
 	product.Cost = product.Cost * ((100 - discountPercentage) / 100)
+}
+
+func main() {
+	pen := Product{100, "Pen", 10, "Stationary"}
+	// fmt.Println(Format(pen))
+	fmt.Println(pen.Format())
+	fmt.Println("After applying 10% discount")
+	// ApplyDiscount(&pen, 10)
+	// (&pen).ApplyDiscount(10)
+	pen.ApplyDiscount(10) //pen is converted to a pointer (receiver expects a pointer) automatically
+	// fmt.Println(Format(pen))
+	fmt.Println(pen.Format())
 }
